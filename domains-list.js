60950090d69558ffa2de0957c7fda5733155b308
Tiisetso Tjabane <tiisetsot@FrankenMac-2.local>
2018-02-07 17:11:25 +0200

    added a new file for the doamins. Created a class that will handle all the user input and it will make the it easier to work with the lib

---
+++ domains-list.js
@@ -0,0 +1,13 @@
+var universalDomains = {
+    "domains" : [
+        "gmail.com",
+        "outlook.com",
+        "yahoo.com",
+        "aol.com",
+        "aim.com",
+        "yandex.com",
+        "protonmail.com",
+        "protonmail.ch",
+        "icloud.com"
+    ]
+}
