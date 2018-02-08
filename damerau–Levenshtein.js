4cfa8a80f3aa2fe394ddf95d49627bf7a1c68148
Cryptic <t.tjabane@gmail.com>
2018-02-08 12:39:44 +0200

    ädded a new distance function

---
+++ damerau–Levenshtein.js
@@ -0,0 +1,10 @@
+'use strict'
+//Author Tiisetso Tjabane
+// This functions will be sused to calculate how similar two strings are.
+//wiki - https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance#Fraud_detection
+
+
+
+function damerauLevenshtein(firstString, secondString){
+    return ""
+}
